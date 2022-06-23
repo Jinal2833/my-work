e78f8346c1d4af23076d49bf101f2e6cf526b6b0
Jinal2833 <107460575+Jinal2833@users.noreply.github.com>
2022-06-23 12:38:52 +0530

    Add files via upload

---
+++ Experience.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Experience() {
+    return (
+        <React.Fragment>
+            <div className="min-h-screen">
+                <div className="container py-5">
+                    <h1 className="text-center mb-5">Experience</h1>
+                    <div className="row">
+                        <div className="col-md-6 offset-md-3">
+                            <div className="card mb-5">
+                                <div className="card-body">
+                                    <h5 className="card-title fw-bold">Full Stack Developer <span className="h6">at Coding Gujju</span></h5>
+                                    <p className="mb-0">APR 2020 - PRESENT</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-6 offset-md-3">
+                            <div className="card mb-3">
+                                <div className="card-body">
+                                    <h5 className="card-title fw-bold">FrontEnd Developer <span className="h6">at Microsoft</span></h5>
+                                    <p className="mb-0">APR 2013 - APR 2016</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default Experience;
